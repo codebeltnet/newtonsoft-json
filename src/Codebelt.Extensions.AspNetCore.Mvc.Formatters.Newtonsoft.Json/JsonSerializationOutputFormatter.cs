8fe5dcc991cbf538b95cb4ddab9172f8af1f3edb
Michael Mortensen <michael@geekle.io>
2024-09-22 20:52:05 +0200

    :twisted_rightwards_arrows: migrated code from Cuemon.Extensions.Newtonsoft.Json incl. unit-test

---
+++ src/Codebelt.Extensions.AspNetCore.Mvc.Formatters.Newtonsoft.Json/JsonSerializationOutputFormatter.cs
@@ -0,0 +1,25 @@
+﻿using Codebelt.Extensions.AspNetCore.Newtonsoft.Json.Converters;
+using Codebelt.Extensions.Newtonsoft.Json.Formatters;
+using Cuemon.AspNetCore.Mvc.Formatters;
+
+namespace Codebelt.Extensions.AspNetCore.Mvc.Formatters.Newtonsoft.Json
+{
+    /// <summary>
+    /// This class handles serialization of objects to JSON using <see cref="NewtonsoftJsonFormatter"/>.
+    /// </summary>
+    public class JsonSerializationOutputFormatter : StreamOutputFormatter<NewtonsoftJsonFormatter, NewtonsoftJsonFormatterOptions>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonSerializationOutputFormatter"/> class.
+        /// </summary>
+        /// <param name="options">The <see cref="NewtonsoftJsonFormatterOptions"/> which need to be configured.</param>
+        public JsonSerializationOutputFormatter(NewtonsoftJsonFormatterOptions options) : base(options)
+        {
+            options.Settings.Converters.AddHttpExceptionDescriptorConverter(o => o.SensitivityDetails = options.SensitivityDetails);
+            foreach (var mediaType in options.SupportedMediaTypes)
+            {
+                SupportedMediaTypes.Add(mediaType.ToString());
+            }
+        }
+    }
+}
---
+++ src/Codebelt.Extensions.AspNetCore.Newtonsoft.Json/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using Codebelt.Extensions.AspNetCore.Newtonsoft.Json.Converters;
+using Codebelt.Extensions.Newtonsoft.Json.Formatters;
+
+namespace Codebelt.Extensions.AspNetCore.Newtonsoft.Json
+{
+    internal static class Bootstrapper
+    {
+        private static readonly object PadLock = new();
+        private static bool _initialized;
+
+        internal static void Initialize()
+        {
+            if (!_initialized)
+            {
+                lock (PadLock)
+                {
+                    if (!_initialized)
+                    {
+                        _initialized = true;
+                        NewtonsoftJsonFormatterOptions.DefaultConverters += list =>
+                        {
+                            list.AddStringValuesConverter();
+                        };
+                    }
+                }
+            }
+        }
+    }
+}
