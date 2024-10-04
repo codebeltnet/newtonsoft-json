bd540af8aa4ebb85b89d7d578f25fc8e2de3cb3f
Michael Mortensen <michael@geekle.io>
2024-10-05 00:43:58 +0200

    :white_check_mark: added additional unittests (ported from Cuemon)

---
+++ test/Codebelt.Extensions.AspNetCore.Mvc.Formatters.Newtonsoft.Json.Tests/Assets/SampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Codebelt.Extensions.AspNetCore.Mvc.Formatters.Newtonsoft.Json.Assets
+{
+    public class SampleModel
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
